1c1fd33c8ff929b11628b161d3719e38ce38e197
Вика <kusyshar@gmail.com>
2020-12-04 01:00:02 +0400

    DialogComplaint

--- app/src/main/java/com/example/petfinder/ComplaintActivity.kt
+++ app/src/main/java/com/example/petfinder/ComplaintActivity.kt
@@ -1,18 +1,24 @@
 package com.example.petfinder
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class ComplaintActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_complaint)
     }
-
     fun toLost (view: View?) {
-        val intent = Intent(this, LostActivity::class.java)
+        val intent = Intent(this, LostActivity::class.java )
         startActivity(intent)
     }
+
+
+    fun showDialog (view: View?) {
+        val dialog = DialogComplaint();
+        dialog.show(supportFragmentManager, "custom")
+    }
+
 }
---
+++ app/src/main/java/com/example/petfinder/DialogComplaint.kt
@@ -0,0 +1,28 @@
+package com.example.petfinder
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.content.Intent
+import android.os.Bundle
+import androidx.annotation.NonNull
+import androidx.fragment.app.DialogFragment
+
+class DialogComplaint: DialogFragment() {
+
+    @NonNull
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val builder = AlertDialog.Builder(activity)
+
+        builder.setTitle("Внимание!")
+        builder.setMessage("Вы действительно хотите оставить жалобу?")
+        builder.setPositiveButton("Да", DialogInterface.OnClickListener(){ dialogInterface: DialogInterface, i: Int ->
+            val intent = Intent(context, LostActivity::class.java )
+            startActivity(intent)
+        })
+        builder.setNegativeButton("Отмена", null)
+        return builder.create()
+
+    }
+
+}
